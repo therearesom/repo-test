98d50789190cffbc5f06fdfb7783ad96b5d18975
boringbug <paradiznik5@gmail.com>
2025-11-08 18:32:56 +0100

    i added something

--- repo-test/src/main.rs
+++ repo-test/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("well hello there!");
+    println!("well hello there!"); // Your code is not good here, ..., you should do this and this
+    println!("Hello how are you");
 }
